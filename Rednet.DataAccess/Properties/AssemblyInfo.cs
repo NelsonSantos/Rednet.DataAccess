c1934bf95ddb69094432dbfa4bfb3aeddf195e23
NelsonSantos <nelson.santos.rp@gmail.com>
2016-12-28 01:02:10 -0200

    Fixed nuget package definitions

--- Rednet.DataAccess/Properties/AssemblyInfo.cs
+++ Rednet.DataAccess/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rednet.DataAccess")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Rednet.DataAccess is yet another component to work with data that simplifies your use.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rednet Software")]
 [assembly: AssemblyProduct("Rednet.DataAccess")]
 [assembly: AssemblyCopyright("Copyright © 2015-2016")]
 [assembly: AssemblyTrademark("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8.*")]
+[assembly: AssemblyVersion("1.0.11.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
